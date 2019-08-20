6a58cd55907dbdb2bee0b79077c9260ea2cc71d4
Rodrigo José Mansilla <rodrigo.mansilla@mercadolibre.com>
2019-08-20 16:56:41 -0300

    Ejercicio tweet con struct

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -1,7 +1,16 @@
 package service
 
-var Tweet string
+import "github.com/RodrigoMansillaMeli/twitterGo/src/domain"
 
-func PublishTweet(tweet string) {
-	Tweet = tweet
+var Tweet domain.Tweet
+
+func PublishTweet(tweet *domain.Tweet) {
+	Tweet.User = tweet.User
+	Tweet.Text = tweet.Text
+	Tweet.Date = tweet.Date
+
+}
+
+func GetTweet() (domain.Tweet) {
+	return Tweet
 }
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -1,17 +1,30 @@
 package service_test
 
 import (
+	"github.com/RodrigoMansillaMeli/twitterGo/src/domain"
 	"github.com/RodrigoMansillaMeli/twitterGo/src/service"
 	"testing"
 )
 
 func TestPublishedTweetIsSaved(t *testing.T) {
+	//Initialization
+	var tweet *domain.Tweet
+	user := "grupoesfera"
+	text := "This is my first tweet"
+	tweet = domain.NewTweet(user, text)
 
-	var tweet string = "This is my first tweet"
-
+	//Operation
 	service.PublishTweet(tweet)
 
-	if service.Tweet != tweet {
-		t.Error("Expected tweet is", tweet)
+	//Validation
+	publishedTweet := service.GetTweet()
+	if publishedTweet.User != user &&
+		publishedTweet.Text != text {
+		t.Errorf("Expected tweet is %s: %s \nbut is %s: %s",
+			user, text, publishedTweet.User, publishedTweet.Text)
+	}
+
+	if publishedTweet.Date == nil {
+		t.Error("Expected date can't be nil")
 	}
 }
